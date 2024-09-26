7f5b239d079b7eab2b40a717e26de1db54229cb1
Tudor Lupu <152184379+DRACULATudor@users.noreply.github.com>
2024-09-26 14:35:16 +0300

    Update WrongAnimal.hpp

--- ex00/WrongAnimal.hpp
+++ ex00/WrongAnimal.hpp
@@ -8,6 +8,8 @@ protected:
     std::string __type;
 public:
     WrongAnimal();
+    WrongAnimal(const WrongAnimal &copy);
+    WrongAnimal &WrongAnimal operator=(const WrongAnimal &assign);
     ~WrongAnimal();
     virtual void makeSound() const;
     virtual std::string gettype() const;
