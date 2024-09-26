62065ad1504f0fb020ddcdba4a297b950bf684ae
Tudor Lupu <152184379+DRACULATudor@users.noreply.github.com>
2024-09-26 14:33:18 +0300

    Update main.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -17,31 +17,31 @@
 // }
 
 // Test case 2
-int main()
-{
-    const Animal *meta = new Animal();
-    const Animal *j = new Dog();
-    const Animal *i = new Cat();
-    std::cout << meta->getType() << " " << std::endl;
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound();
-    j->makeSound();
-    meta->makeSound();
-    delete meta;
-    delete i;
-    delete j;
-}
-
-//Test case 3
 // int main()
 // {
-//     const WrongAnimal *meta = new WrongAnimal();
-//     const WrongAnimal *i = new WrongCat();
-//     std::cout << i->gettype() << " " << std::endl;
-//     std::cout << meta->gettype() << " " << std::endl;
+//     const Animal *meta = new Animal();
+//     const Animal *j = new Dog();
+//     const Animal *i = new Cat();
+//     std::cout << meta->getType() << " " << std::endl;
+//     std::cout << j->getType() << " " << std::endl;
+//     std::cout << i->getType() << " " << std::endl;
 //     i->makeSound();
+//     j->makeSound();
 //     meta->makeSound();
 //     delete meta;
 //     delete i;
+//     delete j;
 // }
+
+// Test case 3
+int main()
+{
+    const WrongAnimal *meta = new WrongAnimal();
+    const WrongAnimal *i = new WrongCat();
+    std::cout << i->gettype() << " " << std::endl;
+    std::cout << meta->gettype() << " " << std::endl;
+    i->makeSound();
+    meta->makeSound();
+    delete meta;
+    delete i;
+}
